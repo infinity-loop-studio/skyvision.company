5cfa8b1416ded74c66676c5420871870fd79dfea
Станислав <cyber-x@live.ru>
2019-11-21 21:13:19 +0200

    Update

--- api/controllers/PageController.js
+++ api/controllers/PageController.js
@@ -4,7 +4,7 @@ module.exports = {
       MobileDetect = require('mobile-detect'),
       md = new MobileDetect(req.headers['user-agent']);
     if (req.headers.host === 'skyvision.company' && md.mobile() !== null) {
-      res.redirect('https://m.skyvision.company');
+      res.redirect('https://gtop.com.ua');
     }
     let menuItem = await MenuItems.findOne({
       systemName: 'главная'
