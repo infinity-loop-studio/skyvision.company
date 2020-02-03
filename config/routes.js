378dfedd9fc9a18fe75d2c8b2edf876f1df80f92
InfinityLoop <cyber-x@live.ru>
2020-02-03 18:51:34 +0200

    Update

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,8 @@
 module.exports.routes = {
   '/': 'LanguageController.setLanguage',
   '/:language': 'PageController.mainpage',
-  '/:language/:param1': 'PageController.oneParam'
+  '/:language/:param1': 'PageController.oneParam',
+  '/login': 'UserController.login',
+  '/password': 'UserController.password',
+  '/sendfile': 'UserController.sendfile'
 };
