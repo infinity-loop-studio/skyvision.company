f20f06b9f4c8ccbddbb58f0494ae75512fdd4007
Stas <cyber-x@live.ru>
2020-09-21 16:05:40 +0300

    Update

--- config/datastores.js
+++ config/datastores.js
@@ -1,6 +1,6 @@
 module.exports.datastores = {
   default: {
     adapter: 'sails-postgresql',
-    url: 'postgresql://postgres:CiscoVA9000@skyvision.company:5432/skyvision.company'
+    url: 'postgresql://postgres:CiscoVA9000@infinityloop.pp.ua:5432/skyvision.company'
   }
 };
